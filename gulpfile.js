6e41687ec3567055e35099ae7893e2aed43b80fa
Scott <girouard.scott@gmail.com>
2018-09-15 16:41:40 -0400

    progress progress progress

--- gulpfile.js
+++ gulpfile.js
@@ -16,22 +16,22 @@ gulp.task('styles', () => {
         .pipe(reload({stream:true}));
 });
 
-gulp.task('js', () => {
-    return browserify('./src/scripts/app.js', { debug: true })
-        .transform('babelify', {
-            sourceMaps: true,
-            presets: ['@babel/preset-env']
-        })
-        .bundle()
-        .on('error', notify.onError({
-            message: "Error: <%= error.message %>",
-            title: 'Error in JS 💀'
-        }))
-        .pipe(source('app.js'))
-        .pipe(buffer())
-        .pipe(gulp.dest('./public/scripts'))
-        .pipe(reload({ stream: true }));
-});
+// gulp.task('js', () => {
+//     return browserify('./src/scripts/app.js', { debug: true })
+//         .transform('babelify', {
+//             sourceMaps: true,
+//             presets: ['@babel/preset-env']
+//         })
+//         .bundle()
+//         .on('error', notify.onError({
+//             message: "Error: <%= error.message %>",
+//             title: 'Error in JS 💀'
+//         }))
+//         .pipe(source('app.js'))
+//         .pipe(buffer())
+//         .pipe(gulp.dest('./public/scripts/'))
+//         .pipe(reload({ stream: true }));
+// });
 
 gulp.task('bs', () => {
 	return browserSync.init({
@@ -42,10 +42,10 @@ gulp.task('bs', () => {
 });
 
 gulp.task('watch',() => {
-    gulp.watch('./src/**/*.js', ['js']);
+   
     gulp.watch('./src/**/*.scss', ['styles']);
     gulp.watch('./public/styles/style.css', reload);
     gulp.watch('./index.html',reload);
 });
 
-gulp.task('default',['styles','js','watch','bs']);
+gulp.task('default',['styles','watch','bs']);
